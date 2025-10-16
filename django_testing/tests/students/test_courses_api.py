2441d941c20fc794c04b5d2fa3f00243109e0e48
kucheryaska <kucheryaska12@gmail.com>
2025-10-16 22:41:30 +0300

    django_3.4_2

--- django_testing/tests/students/test_courses_api.py
+++ django_testing/tests/students/test_courses_api.py
@@ -2,6 +2,10 @@ import pytest
 from rest_framework.test import APIClient
 from model_bakery import baker
 from students.models import Course, Student
+import random
+
+
+
 
 @pytest.fixture
 def client():
@@ -20,64 +24,75 @@ def student_factory():
     return factory
 
 
-@pytest.mark.django_db
-def test_create_first_course(client, course_factory):
+@pytest.mark.django_db(transaction=True)
+def test_create_first_course(client, course_factory, transactional_db=True):
     #Arrange
     course_factory(name = 'first')
 
     #Act
     response = client.get('/api/v1/courses/')
-    data = response.json()
+    first_course = response.json()[0]
     #Assert  
-    assert data[0]['name'] == 'first'
+    assert first_course['name'] == 'first'
+    assert response.status_code == 200
 
 
 
-@pytest.mark.django_db
-def test_get_course_list(client, course_factory):
+@pytest.mark.django_db(transaction=True)
+def test_get_course_list(client, course_factory, transactional_db=True):
     course_factory(_quantity=15)
     response = client.get('/api/v1/courses/')
     data = response.json()
     assert len(data) == 15
+    assert response.status_code == 200
 
 
-@pytest.mark.django_db
-def test_course_filter(client, course_factory):
+@pytest.mark.django_db(transaction=True)
+def test_course_filter(client, course_factory, transactional_db=True):
     course_factory(_quantity=15)
-    response = client.get('/api/v1/courses/?id=22') # id 22, т.к. предыдущие тесты создали 1+15 экземпляров Course.
+    response = client.get('/api/v1/courses/') 
     data = response.json()
-    assert data[0]['id'] == 22
+    id_list = []
+    for course in data:
+        if 'id' in course:
+            id_list.append(course['id'])
+    my_id = random.choice(id_list)
+    new_resp = client.get(f'/api/v1/courses/{my_id}/')
+    assert new_resp.status_code == 200
 
 
-@pytest.mark.django_db
-def test_course_name_filter(client, course_factory):
+@pytest.mark.django_db(transaction=True)
+def test_course_name_filter(client, course_factory, transactional_db=True):
     course_factory(name='second')
     response = client.get('/api/v1/courses/?name=second')
     data = response.json()
     assert data[0]['name'] == 'second'
+    assert response.status_code == 200
 
 
-@pytest.mark.django_db
-def test_course_success_creation(client):
+@pytest.mark.django_db(transaction=True)
+def test_course_success_creation(client, transactional_db=True):
     data = {'name' :'Math', 'students' : []}
     response = client.post('/api/v1/courses/', data=data)
     assert response.status_code == 201
 
 
-@pytest.mark.django_db
-def test_course_success_updating(client, course_factory):
-    course = course_factory(name='Bio')
+@pytest.mark.django_db(transaction=True)
+def test_course_success_updating(client, course_factory, transactional_db=True):
+    course_factory(name='Bio')
+    bio_course = client.get('/api/v1/courses/?name=Bio')
+    bio_course_id = bio_course.json()[0]['id']
     data = {'name' :'Biologiya'}
-    client.post('/api/v1/courses/?name=Bio', data=data)
-    response = client.get('/api/v1/courses/?name=Biologiya')
-    new_name = response.json()
-    assert new_name[0]['name'] == 'Biologiya'
+    client.patch(f'/api/v1/courses/{bio_course_id}/', data=data)
+    response = client.get(f'/api/v1/courses/{bio_course_id}/')
+    assert response.json()['name'] == 'Biologiya'
+    assert response.status_code == 200
 
 
-@pytest.mark.django_db
-def test_course_success_deleting(client, course_factory):
+@pytest.mark.django_db(transaction=True)
+def test_course_success_deleting(client, course_factory, transactional_db=True):
     course_factory(name='Inglish')
     ing_course = client.get('/api/v1/courses/?name=Inglish')
     ing_id = ing_course.json()[0]['id']
-    response = client.delete(f'/api/v1/courses/{ing_id}')
+    response = client.delete(f'/api/v1/courses/{ing_id}/')
     assert response.status_code == 204
